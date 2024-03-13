1c65150aff875c518ecfbf2b44dbf25d36c7a224
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-13 22:00:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
+const inputBox = document.getElementById('inputBox');
 
+const addBtn = document.getElementById('addBtn');
+
+const todoList = document.getElementById('todoList');
+
+
+
+let editTodo = null;
+
+
+
+// Function to add todo
+
+const addTodo = () => {
+
+    const inputText = inputBox.value.trim();
+
+    if (inputText.length <= 0) {
+
+        alert("You must write something in your to do");
+
+        return false;
+
+    }
