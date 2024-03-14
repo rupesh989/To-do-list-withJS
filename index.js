dda15cf3ef19eebedb3ec54c79165ad2c8bd8971
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-14 21:49:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 const inputBox = document.getElementById('inputBox');
-
 const addBtn = document.getElementById('addBtn');
-
 const todoList = document.getElementById('todoList');
-
-
 
 let editTodo = null;
 
-
-
 // Function to add todo
-
 const addTodo = () => {
-
     const inputText = inputBox.value.trim();
-
     if (inputText.length <= 0) {
-
         alert("You must write something in your to do");
-
         return false;
-
     }
+
+    if (addBtn.value === "Edit") {
+        // Passing the original text to editLocalTodos function before edit it in the todoList
+        editLocalTodos(editTodo.target.previousElementSibling.innerHTML);
+        editTodo.target.previousElementSibling.innerHTML = inputText;
+        addBtn.value = "Add";
+        inputBox.value = "";
+    }
+    else {
+        //Creating p tag
+        const li = document.createElement("li");
+        const p = document.createElement("p");
+        p.innerHTML = inputText;
+        li.appendChild(p);
+
+
+        // Creating Edit Btn
+        const editBtn = document.createElement("button");
+        editBtn.innerText = "Edit";
+        editBtn.classList.add("btn", "editBtn");
+        li.appendChild(editBtn);
+
+        // Creating Delete Btn
+        const deleteBtn = document.createElement("button");
+        deleteBtn.innerText = "Remove";
+        deleteBtn.classList.add("btn", "deleteBtn");
+        li.appendChild(deleteBtn);
+
+        todoList.appendChild(li);
+        inputBox.value = "";
+
+        saveLocalTodos(inputText);
+    }
+}
+
