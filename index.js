55c680b0aaa55a742332ae15e02c3f662222eb1b
RUPESH BAVASKAR <rupeshbavaskar24@gmail.com>
2024-03-14 21:49:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -45,4 +45,18 @@ const addTodo = () => {
         saveLocalTodos(inputText);
     }
 }
+// Function to update : (Edit/Delete) todo
+const updateTodo = (e) => {
+    if (e.target.innerHTML === "Remove") {
+        todoList.removeChild(e.target.parentElement);
+        deleteLocalTodos(e.target.parentElement);
+    }
+
+    if (e.target.innerHTML === "Edit") {
+        inputBox.value = e.target.previousElementSibling.innerHTML;
+        inputBox.focus();
+        addBtn.value = "Edit";
+        editTodo = e;
+    }
+}
 
